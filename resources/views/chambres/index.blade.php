a85cc706f99802ad45d7d72b952a4ebc62f276c3
Qasdanovic <oussqas2002@gmail.com>
2025-03-07 21:46:42 +0000

    18- enhance chambres index layout; improve table styling and responsiveness

--- resources/views/chambres/index.blade.php
+++ resources/views/chambres/index.blade.php
@@ -18,59 +18,59 @@
 
     <!-- Make the table responsive -->
     <div class="overflow-x-auto">
-        <table class="min-w-full border border-gray-300 bg-white text-center rounded-md my-3">
+        <table class="min-w-full bg-white rounded-lg overflow-hidden shadow-lg my-5">
             <thead class='text-center'>
-                <tr class="bg-gray-300">
-                    <x-th>id</x-th>
-                    <x-th>numero de chambre</x-th>
-                    <x-th>type</x-th>
-                    <x-th>prix de nuit</x-th>
-                    <x-th>prix de passage</x-th>
-                    <x-th>capacite</x-th>
-                    <x-th>nombre de lits</x-th>
-                    <x-th>etage</x-th>
-                    <x-th>nombre adultes et enfantes</x-th>
-                    <x-th>image</x-th>
-                    <x-th>actions</x-th>
-                </tr>
+            <tr class="bg-gray-300 text-white">
+            <x-th class="px-4 py-3">id</x-th>
+            <x-th class="px-4 py-3">numero de chambre</x-th>
+            <x-th class="px-4 py-3">type</x-th>
+            <x-th class="px-4 py-3">prix de nuit</x-th>
+            <x-th class="px-4 py-3">prix de passage</x-th>
+            <x-th class="px-4 py-3">capacite</x-th>
+            <x-th class="px-4 py-3">nombre de lits</x-th>
+            <x-th class="px-4 py-3">etage</x-th>
+            <x-th class="px-4 py-3">nombre adultes et enfantes</x-th>
+            <x-th class="px-4 py-3">image</x-th>
+            <x-th class="px-4 py-3">actions</x-th>
+            </tr>
             </thead>
             <tbody>
-                {{-- @dd($chambres) --}}
-                @foreach ($chambres as $ch)
-                    <tr class="bg-gray-200 border-gray-400 border-2">
-                        <x-td>{{ $ch->id }}</x-td>
-                        <x-td>{{ $ch->numero_chambre }}</x-td>
-                        <x-td>{{ $ch->type->type_chambre }}</x-td>
-                        <x-td>{{ $ch->tarif->prix_base_nuit }}</x-td>
-                        <x-td>{{ $ch->tarif->prix_base_passage }}</x-td>
-                        <x-td>{{ $ch->capacite->titre_capacite }}</x-td>
-                        <x-td>{{ $ch->capacite->nbr_lits_chambre }}</x-td>
-                        <x-td>{{ $ch->etage_chambre }}</x-td>
-                        <x-td>{{ $ch->capacite->nombre_adultes_enfants_chambre }}</x-td>
-                        <x-td>
-                            <img src="{{ asset('storage/'.$ch->image_chambre) }}" alt="{{ $ch->image_chambre }}">
-                        </x-td>
-                        <x-td class="flex flex-wrap gap-2 justify-center">
-                            <a href="{{ route('chambres.edit', $ch->id)}} ">
-                                    <x-button-edit>
-                                        modifier
-                                    </x-button-edit>
-                                </a>
-                            <form action="{{ route('chambres.destroy', $ch->id) }}" method="post">
-                                @csrf
-                                @method("DELETE")
-                                <x-button-delete>
-                                    supprimer
-                                </x-button-delete>
-                            </form>
-                            <a href="{{ route('chambres.show', $ch->id)}} ">
-                                    <x-button-details>
-                                        details
-                                    </x-button-details>
-                                </a>
-                        </x-td>
-                    </tr>
-                @endforeach
+            @foreach ($chambres as $ch)
+            <tr class="hover:bg-gray-100 border-b border-gray-200 transition-all">
+            <x-td class="px-4 py-3">{{ $ch->id }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->numero_chambre }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->type->type_chambre }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->tarif->prix_base_nuit }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->tarif->prix_base_passage }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->capacite->titre_capacite }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->capacite->nbr_lits_chambre }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->etage_chambre }}</x-td>
+            <x-td class="px-4 py-3">{{ $ch->capacite->nombre_adultes_enfants_chambre }}</x-td>
+            <x-td class="px-4 py-3 w-[150px]">
+                <img src="{{ asset('storage/'.$ch->image_chambre) }}" 
+                 alt="{{ $ch->image_chambre }}"
+                 class="w-full h-20 object-cover rounded">
+            </x-td>
+            <x-td class="px-4 py-3">
+                <div class="flex items-center space-x-2">
+                <a href="{{ route('chambres.edit', $ch->id)}}" title="Modifier">
+                    <x-button-edit>
+                    </x-button-edit>
+                </a>
+                <form action="{{ route('chambres.destroy', $ch->id) }}" method="post">
+                    @csrf
+                    @method("DELETE")
+                    <x-button-delete>
+                    </x-button-delete>
+                </form>
+                <a href="{{ route('chambres.show', $ch->id)}}" title="Details">
+                    <x-button-details>
+                    </x-button-details>
+                </a>
+                </div>
+            </x-td>
+            </tr>
+            @endforeach
             </tbody>
         </table>
         {{ $chambres->links() }}
